f55159d4820f1e693287afedd091951af042ea72
lazaronazareno <lazaronazareno@gmail.com>
2023-04-15 12:33:37 -0300

    header responsive, media queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,15 @@ export default function Header () {
         <img src='https://i.ibb.co/WD3G1LX/Oshi-no-Ko-Logo.png' alt='logo' />
       </a>
       <nav>
-        <ul>
+        <div className='nav-btn'>
+          <label htmlFor='nav-check'>
+            <span />
+            <span />
+            <span />
+          </label>
+        </div>
+        <input title='nav-check' type='checkbox' id='nav-check' />
+        <ul className='nav-links'>
           <li><a href='#home'>Home</a></li>
           <li><a href='#about'>About</a></li>
           <li><a href='#characters'>Characters</a></li>
